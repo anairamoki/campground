a1db6dcb2af7f5f94a1ab1eca1b6c1c2dc9214be
Ariane Moki <ariane.moki@msn.com>
2021-10-27 14:38:02 +0100

    register route logic

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 
@@ -9,10 +10,18 @@ router.get('/register', (req, res) => {
 });
 
 // router to submit the form to
-router.post('/register', async(req, res) => {
-  res.send(req.body);
-
-})
+router.post('/register', catchAsync(async (req, res, next) => {
+  try { //register route logic
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash('success', 'Welcome to Yelp Camp!');
+      res.redirect('/campgrounds');
+  } catch (e) {
+      req.flash('error', e.message);
+      res.redirect('register');
+  }
+}));
 
 
 module.exports = router;

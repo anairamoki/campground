017625d4e9e2eae8a146e4f66dcf271b2d389ad1
Ariane Moki <ariane.moki@msn.com>
2021-11-05 08:54:47 +0000

    add map to show page

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,8 @@
+ //using my own mapbox token
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 6 // starting zoom
+});
